69f3e7d676f1d2dccaa32e500e33eb4edd2252b3
Luca Longo <l.longo@ambita.it>
2020-08-17 19:08:08 +0200

    🐛 Missed select-field red border with errors

--- resources/views/components/form/select-field.blade.php
+++ resources/views/components/form/select-field.blade.php
@@ -19,7 +19,7 @@
                     <button type="button"
                             x-ref="btn"
                             @click="isOn = ! isOn; $nextTick(() => popper.update())"
-                            class="cursor-default relative w-full rounded-md border border-gray-300 bg-white pl-3 pr-10 py-2 text-left focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+                            class="cursor-default relative w-full rounded-md border {{ $errors->has($errorBag) ? 'pr-10 border-red-300 text-red-900 placeholder-red-300 focus:border-red-300 focus:shadow-outline-red ' : 'border-gray-300 focus:shadow-outline-blue focus:border-blue-300' }} bg-white pl-3 pr-10 py-2 text-left focus:outline-none transition ease-in-out duration-150 sm:text-sm sm:leading-5">
                         @if ($multiple)
                             <div class="flex flex-wrap space-x-1">
                                 <template x-if="value == null || value.length == 0">
