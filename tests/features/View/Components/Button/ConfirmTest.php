0f90527e85e6da0239db160c7d7c3cdd3c9419f2
masterix21 <masterix21@users.noreply.github.com>
2020-08-18 17:26:28 +0000

    Fix styling

--- tests/features/View/Components/Button/ConfirmTest.php
+++ tests/features/View/Components/Button/ConfirmTest.php
@@ -1,7 +1,6 @@
 <?php
 namespace Masterix21\XBladeComponents\Tests\features\View\Components\Button;
 
-use Illuminate\Support\Str;
 use Masterix21\XBladeComponents\View\Components\Button\Confirm;
 
 class ConfirmTest extends TestCase
