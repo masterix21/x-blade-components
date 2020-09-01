f4e8785502093840f1e3395e4b859721b8165b8a
Luca Longo <l.longo@ambita.it>
2020-09-01 16:45:17 +0200

    🐛 Unremovable values from select multiple

--- resources/views/components/form/select-field.blade.php
+++ resources/views/components/form/select-field.blade.php
@@ -25,10 +25,10 @@
                         @if ($multiple)
                             <div class="flex flex-wrap space-x-1">
                                 <template x-if="value == null || value.length == 0">
-                                    <div>-</div>
+                                    <div>{{ $placeholder ?: '-' }}</div>
                                 </template>
                                 <template x-for="key in value" :key="key">
-                                    <div class="rounded-md px-2 text-xs bg-gray-200 text-gray-500" x-text="options[key]">icao</div>
+                                    <div class="rounded-md px-2 text-xs bg-gray-200 text-gray-500" x-text="options[key]"></div>
                                 </template>
                             </div>
                         @else
@@ -47,7 +47,9 @@
             <ul tabindex="-1" role="listbox" aria-labelledby="listbox-label" aria-activedescendant="listbox-item-3" class="max-h-60 rounded-md py-1 text-base leading-6 shadow-xs overflow-auto focus:outline-none sm:text-sm sm:leading-5">
                 @foreach ($options as $key => $label)
                     <li class="text-gray-900 cursor-pointer select-none relative py-2 pl-3 pr-9 hover:bg-gray-50"
-                        @if (! $disabled && ! $readOnly) x-on:click="@if ($multiple) value.indexOf('{{ $key }}') >= 0 ? value.splice(value.indexOf({{ json_encode($key) }}), 1) : value.push({{ json_encode($key) }}); @else value = {{ json_encode($key) }};@endif $dispatch('input', value)" @endif>
+                        @if (! $disabled && ! $readOnly)
+                            x-on:click="@if ($multiple) value.indexOf({{ json_encode($key) }}) >= 0 ? value.splice(value.indexOf({{ json_encode($key) }}), 1) : value.push({{ json_encode($key) }});@else value = {{ json_encode($key) }};@endif $dispatch('input', value)"
+                        @endif >
                                 <span class="block truncate"
                                       @if ($multiple)
                                         :class="{'font-normal': ! value.indexOf({{ json_encode($key) }}) >= 0, 'font-semibold': value.indexOf({{ json_encode($key) }}) >= 0}"
