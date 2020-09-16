76e9f2aa71241d9afee8d777c6097ce735e192d7
Luca Longo <l.longo@ambita.it>
2020-09-16 17:20:56 +0200

    Money field value aligned to right

--- resources/views/components/form/money-field.blade.php
+++ resources/views/components/form/money-field.blade.php
@@ -16,7 +16,7 @@
     </x-slot>
 
     <input @if ($id ?? false) id="{{ $id }}" @endif
-           class="form-input block w-full @if (! blank($currency) || ! blank($prepend ?? null) || ! blank($append ?? null)) rounded-none @endif @if (blank($currency) && blank($prepend ?? null)) rounded-l-md @else border-l-0 @endif @if (blank($append ?? null)) rounded-r-md @else border-r-0 @endif transition ease-in-out duration-150 sm:text-sm sm:leading-5 {{ $errors->has($errorBag) ? 'pr-10 border-red-300 text-red-900 placeholder-red-300 focus:border-red-300 focus:shadow-outline-red ' : '' }} {{ $readOnly ? 'bg-gray-50 text-gray-500' : '' }}"
+           class="form-input block w-full text-right @if (! blank($currency) || ! blank($prepend ?? null) || ! blank($append ?? null)) rounded-none @endif @if (blank($currency) && blank($prepend ?? null)) rounded-l-md @else border-l-0 @endif @if (blank($append ?? null)) rounded-r-md @else border-r-0 @endif transition ease-in-out duration-150 sm:text-sm sm:leading-5 {{ $errors->has($errorBag) ? 'pr-10 border-red-300 text-red-900 placeholder-red-300 focus:border-red-300 focus:shadow-outline-red ' : '' }} {{ $readOnly ? 'bg-gray-50 text-gray-500' : '' }}"
            {{ $attributes }}
            x-data
            x-ref="input"
